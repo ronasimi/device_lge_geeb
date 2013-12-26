6e6222730d5a7f7c6803b083244d6b6498001443
edgar.benson <edbenson@hotmail.com>
2013-12-25 20:19:03 -0800

    Build CM

---
+++ cm.mk
@@ -0,0 +1,27 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 768
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/geeb/full_geeb.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := geeb
+PRODUCT_NAME := cm_geeb
+PRODUCT_BRAND := LGE
+PRODUCT_MODEL := LG Optimus G
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=geeb BUILD_FINGERPRINT=google/occam/geeb:4.3/JWR66V/737497:user/release-keys PRIVATE_BUILD_DESC="occam-user 4.3 JWR66V 737497 release-keys"
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
